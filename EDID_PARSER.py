38f07474d2556737dcc405ad728ab6dc87b5c8c2
karan <karan@example.com>
2025-12-19 20:32:16 +0530

    EDID raw dumping

--- EDID_PARSER.py
+++ EDID_PARSER.py
@@ -7,28 +7,35 @@ import argparse
 
 parser = ArgumentParser(
                     prog='EDID Parser',
-                    description=r'This parses the provided EDID gives the required details, give the EDID path <SYSTEM\CurrentControlSet\Enum\DISPLAY\AOC3402\4&20696d67&0&UID24644\Device Parameters>',
+                    description=r"""This parses the provided EDID gives the required details, give the EDID path <SYSTEM\CurrentControlSet\Enum\DISPLAY\AOC3402\4&20696d67&0&UID24644\Device Parameters>
+                    
+                    Required Arguments:
+                    1. --EDID_Path
+
+                    Option Arguments:
+                    1. --dump
+
+                    """,
                     epilog='-------Thanks-------')
 
+parser.add_argument('--EDID_Path', type=str)
+parser.add_argument('--dump', action="store_true" )
+parser.add_argument('--filename')
 
-class DumpEDID(Action):
-    def __init__(self, option_strings, dest, bytes, nargs=None, **kwargs):
-        if nargs is not None:
-            raise ValueError("nargs not allowed")
-        super().__init__(option_strings, dest, **kwargs)
-    def __call__(self, parser, namespace, values, option_string=None):
-        print('%r %r %r %r' % (namespace, values, bytes, option_string))
-        setattr(namespace, self.dest, values)
-        with open(f"{values}" "wb" ) as f:
-            for i in bytes:
-                f.write(i)
-
-parser.add_argument('-EDID_Path', type=str)
-parser.add_argument('--dump', action=DumpEDID)
-
-
+args = parser.parse_args()
+EDID_Path = args.EDID_Path
+Dump_Name = args.filename
 
 def EDID_Collector(EDID_Path):
     Keyobject = (winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, EDID_Path, access=winreg.KEY_READ))
     EDID =winreg.EnumValue(Keyobject, 0)[1]
     return EDID
+
+EDID = EDID_Collector(EDID_Path)
+
+def BinaryDump(EDID, filename='dump'):
+    with open(filename, "wb") as f:
+        f.write(EDID)
+
+if args.dump:
+    BinaryDump(EDID_Collector(EDID_Path), Dump_Name )
