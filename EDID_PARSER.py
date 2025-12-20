42c0fb2e692b3ceb33f11bb8ab594439ecd309da
karan <karan@example.com>
2025-12-20 12:56:10 +0530

    Started working on Display specifications [Product Code, Product name

--- EDID_PARSER.py
+++ EDID_PARSER.py
@@ -1,9 +1,6 @@
 from argparse import *
 import struct
 import winreg
-import re
-from pathlib import Path
-import argparse
 
 parser = ArgumentParser(
                     prog='EDID Parser',
@@ -26,22 +23,50 @@ args = parser.parse_args()
 EDID_Path = args.EDID_Path
 Dump_Name = args.filename
 
-
+# EDID Collection and validate the EDID
 def EDID_Collector(EDID_Path):
     Keyobject = (winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, EDID_Path, access=winreg.KEY_READ))
     EDID =winreg.EnumValue(Keyobject, 0)[1]
     Header = struct.unpack("<8B",EDID[0:8])
-    if ((Header[0] & Header[-1]) == 0) & (sum(Header[1:7]) == 1530):
+    if ((Header[0] & Header[-1]) == 0) & ((Header[1:7]) == (255,255,255,255,255,255)):
         return EDID
     else:
         print("Incorrect EDID format")
 
 EDID = EDID_Collector(EDID_Path)
-print(EDID)
 
+# Dump EDID
 def BinaryDump(EDID, filename='dump'):
     with open(filename, "wb") as f:
         f.write(EDID)
 
 if args.dumpEDID:
     BinaryDump(EDID_Collector(EDID_Path), Dump_Name )
+
+
+def display_spec(EDID):
+
+    # Product Code
+    Product_Code_bytes = struct.unpack("<H", EDID[8:10])[0]
+    Product_Code = ''.join([
+        chr(((Product_Code_bytes >> 10) & 0x1F) +64),
+        chr(((Product_Code_bytes >> 5) & 0x1F) +64),
+        chr((Product_Code_bytes & 0x1F) + 64)
+    ])
+
+    # Product Serial Number \\ Product Named
+    Display_Descriptor = struct.unpack('<18B', EDID[72:90])
+    ProductSerial_Number = ''
+    ProductName = ''
+    if (Display_Descriptor[0:3]) == (0x00,0x00,0x00) and (Display_Descriptor[3] == 0xFF):
+        for a in Display_Descriptor[5:18]:
+            ProductSerial_Number += chr(a)
+    elif (Display_Descriptor[0:3]) == (0x00,0x00,0x00) and (Display_Descriptor[3] == 0xFE):
+        for b in Display_Descriptor[5:18]:
+            if b == 0x0A:
+                break
+            ProductName += chr(b)
+
+    return (Product_Code, ProductName)
+
+print(display_spec(EDID))
